97bfe5ac4c93032bf6544e27e1d369f87db88269
Ariana Hwang <ajhwang0717@gmail.com>
2019-09-09 15:25:44 -0400

    arrow functions commit

---
+++ arrow-functions-practice.js
@@ -0,0 +1,15 @@
+//Arrow Functions
+
+//alternative way #1
+
+const timesFive = (x) =>{
+    return x * 5;
+}
+
+//alternative way #2
+
+const timesFive = (x) => x * 5;
+
+//alternative way #3
+
+const timesFive x => x * 5;
